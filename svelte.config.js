00382766859037074b668ec54f526389a1683397
kiwixz <kiwixz@outlook.com>
2026-01-16 21:16:32 +0100

    Minify HTML

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,22 @@
 import adapter from '@sveltejs/adapter-static'
+import htmlMinifier from 'sveltekit-html-minifier'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  kit: { adapter: adapter() },
+  kit: {
+    adapter: htmlMinifier(adapter(), {
+      minifierOptions: {
+        collapseBooleanAttributes: true,
+        collapseWhitespace: true,
+        conservativeCollapse: true,
+        minifyURLs: url => url.replace(/^\.\//, ''),
+        preserveLineBreaks: true,
+        processConditionalComments: true,
+        removeOptionalTags: true,
+        removeRedundantAttributes: true,
+      },
+    }),
+  },
 }
 
 export default config
