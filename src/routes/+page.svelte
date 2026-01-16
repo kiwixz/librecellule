e14f131c9e3b9ae5e74a8cec96b1b51c3f04e5f9
kiwixz <kiwixz@outlook.com>
2026-01-16 21:55:37 +0100

    Add title and background

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,6 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<svelte:head>
+  <title>LibreCellule</title>
+</svelte:head>
+
+<div class="h-dvh bg-green-900">
+</div>
