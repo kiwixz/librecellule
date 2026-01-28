3f48f58f077e6e23caabe036109039d102180d78
kiwixz <kiwixz@outlook.com>
2026-01-28 17:56:15 +0100

    Skip seed prerender and color it

--- src/routes/(root)/+page.svelte
+++ src/routes/(root)/+page.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
+  import { browser } from '$app/environment'
   import Card from '$lib/card.svelte'
   import { Generator } from '$lib/random'
 
-  let generator = new Generator()
+  if (browser)
+    var generator = new Generator()
 </script>
 
-<div class="h-dvh bg-green-900">
+<div class="h-dvh bg-green-900 text-green-200">
   <main>
     <Card rank=7 suit=1 />
-
-    <div class="absolute bottom-0 right-0">{generator.state()}</div>
   </main>
+
+  <div class="absolute bottom-0 right-0">
+    {generator?.state()}
+  </div>
 </div>
