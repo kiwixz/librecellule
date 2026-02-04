e41f489db4abebbccb4bac6bd04907aa1dde5def
kiwixz <kiwixz@outlook.com>
2026-02-04 02:14:23 +0100

    Add JSON minifier

---
+++ src/app.d.ts
@@ -0,0 +1,12 @@
+declare module '*.json?min' {
+  const src: string
+  export default src
+}
+declare module '*.json?min&inline' {
+  const src: string
+  export default src
+}
+declare module '*.json?min&no-inline' {
+  const src: string
+  export default src
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,49 @@
-import { defineConfig } from 'vite'
+import fs from 'node:fs/promises'
+import path from 'node:path'
+import type { Plugin, ResolvedConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
+import JSON5 from 'json5'
 
-export default defineConfig({ plugins: [sveltekit(), tailwindcss()] })
+function jsonMinifier(): Plugin {
+  let config: ResolvedConfig
+
+  return {
+    name: 'json-minifier',
+    enforce: 'pre',
+
+    configResolved(resolvedConfig) {
+      config = resolvedConfig
+    },
+    resolveId: {
+      filter: {
+        id: /\.json\?min(?:&(?:no-)?inline)?$/,
+      },
+      async handler(source, importer) {
+        const extraQuery = source.match(/\?min(.*)/)![1]
+
+        let file = source.replace(/\?.*/, '')
+        if (importer)
+          file = path.resolve(path.dirname(importer), file)
+
+        let json = await fs.readFile(file, 'utf8')
+        json = JSON.stringify(JSON5.parse(json))
+
+        const minified = `${config.cacheDir}/${path.relative(config.root, file)}`
+        await fs.mkdir(path.dirname(minified), { recursive: true })
+        await fs.writeFile(minified, json)
+
+        return `${minified}?url${extraQuery}`
+      },
+    },
+  }
+}
+
+export default {
+  plugins: [
+    sveltekit(),
+    tailwindcss(),
+
+    jsonMinifier(),
+  ],
+}
