dbc30217a91c448f7f9c19476f39de18114ba804
kiwixz <kiwixz@outlook.com>
2026-02-04 23:14:58 +0100

    Add manifest

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
   import './layout.css'
+  import manifest from '$lib/manifest.json?min'
 
   let { children } = $props()
 </script>
+
+<svelte:head>
+  <link rel="manifest" href={manifest} />
+</svelte:head>
 
 {@render children()}
