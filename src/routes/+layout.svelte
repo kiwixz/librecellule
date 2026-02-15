f011f768dd1b0872102c4bc8b890157068871253
kiwixz <kiwixz@outlook.com>
2026-02-15 02:49:27 +0100

    Move manifest to a stable URL

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
   import './layout.css'
-  import manifest from '$lib/manifest.json?min'
 
   let { children } = $props()
 </script>
-
-<svelte:head>
-  <link rel="manifest" href={manifest} />
-</svelte:head>
 
 {@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,40 +1,31 @@
 import fs from 'node:fs/promises'
-import path from 'node:path'
-import type { Plugin, ResolvedConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
 import tailwindcss from '@tailwindcss/vite'
 import JSON5 from 'json5'
+import type { Plugin, ResolvedConfig } from 'vite'
 
 function jsonMinifier(): Plugin {
   let config: ResolvedConfig
 
   return {
     name: 'json-minifier',
-    enforce: 'pre',
 
     configResolved(resolvedConfig) {
       config = resolvedConfig
     },
-    resolveId: {
-      filter: {
-        id: /\.json\?min(?:&(?:no-)?inline)?$/,
-      },
-      async handler(source, importer) {
-        const extraQuery = source.match(/\?min(.*)/)![1]
 
-        let file = source.replace(/\?.*/, '')
-        if (importer)
-          file = path.resolve(path.dirname(importer), file)
+    async closeBundle(error) {
+      if (error)
+        return
 
-        let json = await fs.readFile(file, 'utf8')
-        json = JSON.stringify(JSON5.parse(json))
-
-        const minified = `${config.cacheDir}/${path.relative(config.root, file)}`
-        await fs.mkdir(path.dirname(minified), { recursive: true })
-        await fs.writeFile(minified, json)
-
-        return `${minified}?url${extraQuery}`
-      },
+      Promise.all((await fs.readdir(config.build.outDir, { recursive: true }))
+        .filter(file => /\.(?:json5?|webmanifest)$/.test(file))
+        .map(async (file) => {
+          const path = `${config.build.outDir}/${file}`
+          let json = await fs.readFile(path, 'utf8')
+          json = JSON.stringify(JSON5.parse(json))
+          await fs.writeFile(path, json)
+        }))
     },
   }
 }
