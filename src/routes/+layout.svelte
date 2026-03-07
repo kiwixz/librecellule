0b1d228b3698e6c20255041df000b6ebd34b46ce
kiwixz <kiwixz@outlook.com>
2026-03-07 21:34:08 +0100

    Add JSON-LD

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,22 @@
 <script lang="ts">
   import './layout.css';
 
-  let { children } = $props();
+  const { children } = $props();
+
+  const jsonld = {
+    '@context': 'https://schema.org',
+    '@type': 'WebApplication',
+    'name': 'LibreCellule',
+    'applicationCategory': 'GameApplication',
+    'operatingSystem': 'Web',
+    'offers': {
+      '@type': 'Offer',
+      'price': 0,
+    },
+  };
 </script>
 
 {@render children()}
+
+<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+{@html `<script type="application/ld+json">${JSON.stringify(jsonld)}</script >` }
