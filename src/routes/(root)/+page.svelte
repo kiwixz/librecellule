333cfa297e1c9bb5498107b4b7eb57aec77e4c84
kiwixz <kiwixz@outlook.com>
2026-05-17 20:45:07 +0200

    Update buttons

--- src/routes/(root)/+page.svelte
+++ src/routes/(root)/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import { browser } from '$app/environment';
+  import Menu from '@lucide/svelte/icons/menu';
   import Undo from '@lucide/svelte/icons/undo-2';
   import Redo from '@lucide/svelte/icons/redo-2';
   import Shuffle from '@lucide/svelte/icons/shuffle';
@@ -28,24 +29,29 @@
   </main>
 
   <div class="absolute bottom-0 p-1 w-full flex flex-wrap gap-2 items-end">
-    <div class="flex max-sm:flex-col-reverse flex-wrap gap-1">
-      <button class="btn" onclick={() => game.reset()}>
-        <Shuffle /> New Deal
-      </button>
-      <div class="join">
-        <button class="btn join-item" disabled={!game.canUndo()} onclick={() => game.undo()}>
-          <Undo /> Undo
-        </button>
-        {#if game.canRedo()}
-          <button class="btn join-item" onclick={() => game.redo()}>
-            <Redo /> Redo
-          </button>
-        {/if}
+    <div class="dropdown dropdown-top">
+      <div tabindex="0" role="button" class="btn btn-square" aria-label="Menu"><!-- safari cant focus buttons -->
+        <Menu />
       </div>
+
+      <ul class="mb-1 w-40 dropdown-content bg-base-200 rounded-box shadow menu">
+        <li>
+          <button onclick={() => game.reset()}>
+            <Shuffle /> New Deal
+          </button>
+        </li>
+      </ul>
     </div>
 
-    <div class="ml-auto leading-none break-all">
-      {game.seed}
+    <div class="ml-auto flex flex-col flex-wrap gap-1">
+      {#if game.canRedo()}
+        <button class="btn" onclick={() => game.redo()}>
+          <Redo /> Redo
+        </button>
+      {/if}
+      <button class="btn" disabled={!game.canUndo()} onclick={() => game.undo()}>
+        <Undo /> Undo
+      </button>
     </div>
   </div>
 </div>
