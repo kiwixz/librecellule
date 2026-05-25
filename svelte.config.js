d35c424de5dce77dfc24a8755e7b18ca51a673c7
kiwixz <kiwixz@outlook.com>
2026-05-25 19:49:40 +0200

    Customize version string

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,16 @@
+import { execSync } from 'node:child_process';
 import adapter from '@sveltejs/adapter-static';
+
+function version() {
+  const date = new Date(execSync('git show -s --format=%cI').toString().trim());
+  const commit = execSync('git rev-parse --short @').toString().trim();
+
+  const year = date.getUTCFullYear().toString().padStart(4, '0');
+  const month = (date.getUTCMonth() + 1).toString().padStart(2, '0');
+  const day = date.getUTCDate().toString().padStart(2, '0');
+
+  return `${year}${month}${day}-${commit}`;
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
@@ -8,5 +20,8 @@ export default {
   kit: {
     adapter: adapter(),
     inlineStyleThreshold: Infinity,
+    version: {
+      name: version(),
+    },
   },
 };
