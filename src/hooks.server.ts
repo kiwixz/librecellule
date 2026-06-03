6e27dd4b92f5201d2cac43a0e34013801202623b
kiwixz <kiwixz@outlook.com>
2026-06-04 00:41:14 +0200

    Switch HTML CSS minifier to Lightning CSS

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,13 +2,27 @@ import type { Options as MinifierOptions } from 'html-minifier-terser';
 
 import { building } from '$app/environment';
 import { minify } from 'html-minifier-terser';
+import { transform as transformCss } from 'lightningcss';
+
+function minifyCss(css: string): string {
+  let code: Uint8Array<ArrayBufferLike> = new TextEncoder().encode(css);
+
+  code = transformCss({
+    filename: '',
+    code,
+    minify: true,
+    errorRecovery: true,
+  }).code;
+
+  return new TextDecoder().decode(code);
+}
 
 const minifierOptions: MinifierOptions = {
   collapseBooleanAttributes: true,
   collapseWhitespace: true,
   conservativeCollapse: true,
   decodeEntities: true,
-  minifyCSS: true,
+  minifyCSS: (css, type) => type ? css : minifyCss(css),
   minifyJS: true,
   minifyURLs: url => url.replace(/^\.\//, ''),
   preserveLineBreaks: true,
